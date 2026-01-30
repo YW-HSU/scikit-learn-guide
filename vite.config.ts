873ea0695079d7cdd77638793e1f62dde1ddb673
YW-HSU <hsu20060616@gmail.com>
2026-01-30 20:23:22 +0800

    feat: build dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/scikit-learn-guide/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
